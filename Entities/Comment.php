9b17692e54d2f4aa685d21cc08409caa68d331c2
Aseel Haikal <aseelHaikal11@gmail.com>
2021-08-05 23:13:33 +0300

    first commit

---
+++ Entities/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\CommentModule\Entities;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\ProductModule\Entities\Product;
+
+class Comment extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'content',
+        'product_id',
+        'user_id'
+    ];
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('commentmodule::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('commentmodule.name') !!}
+    </p>
+@endsection
